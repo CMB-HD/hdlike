c41f354705730a94c11816f3eb9fb7ab0f74bbd2
Amanda MacInnis <amandamacinnis92@gmail.com>
2023-08-18 10:42:35 -0700

    updated setup script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="hdlike",
     version="1.0",
     description="Likelihood for CMB-HD",
-    url="https://github.com/amandamacinnis/hdlike",
+    url="https://github.com/CMB-HD/hdlike",
     author="CMB-HD Collaboration",
     python_requires=">=3",
     install_requires=["numpy"],
